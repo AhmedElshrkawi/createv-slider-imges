e366c1899870513f8ecff136e27903619af8302f
ahmedelsharkaye395@gmail.com <ahmedelsharkaye395@gmail.com>
2024-10-24 17:52:04 +0300

    createv slider imges

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var img = document.querySelector('.img');
+var cont = document.querySelector('.container');
+
+
+function phons(phon){
+
+    img.src = phon;
+}
+
+function colors(color){
+
+    cont.style.background = color
+}
